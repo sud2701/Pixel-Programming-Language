908ba6f7faa47fe1c0ec1ff57eb2772befd9ef48
mpate133 <mpate133@asu.edu>
2023-04-29 16:43:17 -0700

    Changing some logic of for_loop evaluation and fixing infinite loop

--- src/parser.pl
+++ src/parser.pl
@@ -16,17 +16,17 @@ block_evaluation(code_block(SubBlock), PrevEnv, Env):-
 
 block_evaluation(empty(), _, _).
 
-sub_block(sub_block(Dec, Rest)) --> declaration(Dec), sub_block(Rest).
-sub_block(sub_block(Cmd)) --> leftRecursionRemovedCommand(Cmd).
 sub_block(sub_block(Dec)) --> declaration(Dec).
+sub_block(sub_block(Dec, Rest)) --> declaration(Dec), sub_block(Rest).
 sub_block(sub_block(Cmd, Rest)) --> leftRecursionRemovedCommand(Cmd), sub_block(Rest).
+sub_block(sub_block(Cmd)) --> leftRecursionRemovedCommand(Cmd).
 sub_block([]) --> [].
 
-sub_block_evaluation(sub_block(declaration_node(Dec), Rest), PrevEnv, Env) :- declaration_evaluation(Dec, PrevEnv, TempEnv), sub_block_evaluation(Rest, TempEnv, Env).
+sub_block_evaluation(sub_block(Dec, Rest), PrevEnv, Env) :- declaration_evaluation(Dec, PrevEnv, TempEnv), sub_block_evaluation(Rest, TempEnv, Env).
 sub_block_evaluation(sub_block(Cmd, Rest), PrevEnv, Env) :- command_evaluation(Cmd, PrevEnv, TempEnv), sub_block_evaluation(Rest, TempEnv, Env).
-sub_block_evaluation(sub_block(declaration_node(Dec)), PrevEnv, Env) :- declaration_evaluation(Dec, PrevEnv, Env).
+sub_block_evaluation(sub_block(Dec), PrevEnv, Env) :- declaration_evaluation(Dec, PrevEnv, Env).
 sub_block_evaluation(sub_block(Cmd), PrevEnv, Env) :- command_evaluation(Cmd, PrevEnv, Env).
-sub_block_evaluation().
+sub_block_evaluation([], Env, Env).
 
 declaration(Dec) --> const_declaration(Dec).
 declaration(Dec) --> var_declaration(Dec).
@@ -59,21 +59,25 @@ const_declaration(const_declaration(const, Dtype, Iden, =, Expr, ;, Rest)) --> [
 const_declaration(const_declaration(const, Dtype, Iden, =, Bool, ;, Rest)) --> [const], variable_datatype(Dtype), variable(Iden), [=], booleanCondition(Bool), [;], declaration(Rest).
 const_declaration(const_declaration(const, Dtype, Iden, =, Expr, ;, Rest)) --> [const], variable_datatype(Dtype), variable(Iden), [=], ternary_expression(Expr), [;], declaration(Rest).
 
-is_const_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, const, Datatype, N) | Env]) :-
-    extract_value(Val, PrevEnv, Env, N),
-    \+ member((Iden, _, _, _), Env),
+is_const_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, const, Datatype, N) | TempEnv]) :-
+    evaluate_extract(Val, PrevEnv, TempEnv, N),
+    \+ member((Iden, _, _, _), TempEnv),
     ( valid_datatype_value(Datatype, N)
     -> true
     ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail
     ).
 
-is_const_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, const, Datatype, N) | PrevEnv]) :-
-    extract_value(Val, PrevEnv, _, N),
-    \+ member((Iden, _, _, _), PrevEnv),
-    ( valid_datatype_value(Datatype, N)
-    -> true
-    ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail
-    ).
+evaluate_extract(Val, PrevEnv, Env, N) :-
+    extract_value(Val, PrevEnv, TempEnv, N),
+    (var(TempEnv) -> Env = PrevEnv; Env = TempEnv).
+
+% is_const_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, const, Datatype, N) | PrevEnv]) :-
+%     extract_value(Val, PrevEnv, _, N),
+%     \+ member((Iden, _, _, _), PrevEnv),
+%     ( valid_datatype_value(Datatype, N)
+%     -> true
+%     ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail
+%     ).
 
 var_declaration(var_declaration(var, Dtype, Iden, ;)) --> [var], variable_datatype(Dtype), variable(Iden), [;].
 var_declaration(var_declaration(var, Dtype, Iden, =, Expr, ;)) --> [var], variable_datatype(Dtype), variable(Iden), [=], simple_expression(Expr), [;].
@@ -85,20 +89,19 @@ var_declaration(var_declaration(var, Dtype, Iden, =, Expr, ;, Rest)) --> [var], 
 is_variable_declared(variable(Iden), datatype(Datatype), PrevEnv, [(Iden, var, Datatype, _) | PrevEnv]) :-
     (\+ member((Iden, _, _, _), PrevEnv); throw(error(variable_already_declared, [Iden]))).
 
-is_variable_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, var, Datatype, N) | PrevEnv]) :-
-    extract_value(Val, PrevEnv, Env, N),
-    PrevEnv = Env,
-    (\+ member((Iden, _, _, _), PrevEnv)
+is_variable_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, var, Datatype, N) | TempEnv]) :-
+    evaluate_extract(Val, PrevEnv, TempEnv, N),
+    (\+ member((Iden, _, _, _), TempEnv)
     -> ( valid_datatype_value(Datatype, N)
     -> true ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail)
     ; throw(error(variable_already_declared, [Iden]))).
 
-is_variable_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, var, Datatype, N) | Env]) :-
-    extract_value(Val, PrevEnv, Env, N),
-    (\+ member((Iden, _, _, _), Env)
-    -> ( valid_datatype_value(Datatype, N)
-    -> true ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail)
-    ; throw(error(variable_already_declared, [Iden]))).
+% is_variable_declared(variable(Iden), datatype(Datatype), Val, PrevEnv, [(Iden, var, Datatype, N) | Env]) :-
+%     extract_value(Val, PrevEnv, Env, N),
+%     (\+ member((Iden, _, _, _), Env)
+%     -> ( valid_datatype_value(Datatype, N)
+%     -> true ;  format('Error: invalid value "~w" for datatype "~w"~n', [N, Datatype]), fail)
+%     ; throw(error(variable_already_declared, [Iden]))).
 
 variable_datatype(datatype(int)) --> [int].
 variable_datatype(datatype(bool)) --> [bool].
@@ -136,6 +139,10 @@ leftRecursionRemovedCommand(for_loop(for, '(', Asg, ;, Bool, ;, Expr, ')', Cmd, 
 
 leftRecursionRemovedCommand(for_loop_range(Iden, in, range, From, To, Jump, Cmd)) --> [for], variable(Iden), [in], [range], ['('], simple_expression(From), [','], simple_expression(To), [','], simple_expression(Jump), [')'], leftRecursionRemovedCommand(Cmd).
 leftRecursionRemovedCommand(for_loop_range_single(Iden, in, range, To, Cmd)) --> [for], variable(Iden), [in], [range], ['('], simple_expression(To), [')'], leftRecursionRemovedCommand(Cmd).
+
+leftRecursionRemovedCommand(for_loop_range(Iden, in, range, From, To, Jump, Cmd, Cmd1)) --> [for], variable(Iden), [in], [range], ['('], simple_expression(From), [','], simple_expression(To), [','], simple_expression(Jump), [')'], leftRecursionRemovedCommand(Cmd), leftRecursionRemovedCommand(Cmd1).
+leftRecursionRemovedCommand(for_loop_range_single(Iden, in, range, To, Cmd, Cmd1)) --> [for], variable(Iden), [in], [range], ['('], simple_expression(To), [')'], leftRecursionRemovedCommand(Cmd), leftRecursionRemovedCommand(Cmd1).
+
 leftRecursionRemovedCommand(if_else(if, Bool, Cmd, else, Cmd1)) --> [if], ['('], booleanCondition(Bool), [')'], leftRecursionRemovedCommand(Cmd), [else], leftRecursionRemovedCommand(Cmd1).
 leftRecursionRemovedCommand(if_else_if(if, Bool, Cmd, Rest)) --> [if], ['('], booleanCondition(Bool), [')'], leftRecursionRemovedCommand(Cmd),  else_if_ladder(Rest).
 leftRecursionRemovedCommand(if(if, Bool, Cmd)) --> [if], ['('], booleanCondition(Bool), [')'], leftRecursionRemovedCommand(Cmd).
@@ -160,6 +167,26 @@ leftRecursionRemovedCommand(divide_(Iden, /, =, Expr)) --> variable(Iden), [/], 
 leftRecursionRemovedCommand(divide_(Iden, /, =, Expr, Cmd)) --> variable(Iden), [/], [=], simple_expression(Expr), [;], leftRecursionRemovedCommand(Cmd).
 leftRecursionRemovedCommand(command_block(Blk)) --> block(Blk).
 
+update_variables([], PrevEnv, PrevEnv).
+update_variables([(Var, Type, Datatype, Val)|Rest], PrevEnv, UpdatedEnv) :-
+    (select((Var, Type, Datatype, _), PrevEnv, _) ->
+        replace(PrevEnv, (Var, Type, Datatype, _), (Var, Type, Datatype, Val), NewTempEnv)
+    ;
+        NewTempEnv = PrevEnv
+    ),
+    update_variables(Rest, NewTempEnv, UpdatedEnv).
+
+replace_variable((Var, Type, Datatype, Val), [], [(Var, Type, Datatype, Val)]).
+replace_variable((Var, Type, Datatype, Val), [(Var, Type, Datatype, _)|Rest], [(Var, Type, Datatype, Val)|Rest]).
+replace_variable((Var, Type, Datatype, Val), [Head|Rest], [Head|NewRest]) :-
+    Head \= (Var, Type, Datatype, _),
+    replace_variable((Var, Type, Datatype, Val), Rest, NewRest).
+
+
+evaluate_command_env(Cmd, PrevEnv, Env) :-
+    command_evaluation(Cmd, PrevEnv, TempEnv),
+    (var(TempEnv) -> Env = PrevEnv; Env = TempEnv).
+
 command_evaluation(command_assign(T), PrevEnv, Env) :-
     assignment_evalutation(T, PrevEnv, Env).
 
@@ -169,19 +196,131 @@ command_evaluation(command_assign(T, Cmd), PrevEnv, Env) :-
 
 command_evaluation(for_loop(for, '(', Dec, Bool, ;, Expr, ')', Cmd), PrevEnv, Env):-
     declaration_evaluation(Dec, PrevEnv, TempEnv),
-    for_loop_evaluation(Bool, Expr, Cmd, TempEnv, Env).
+    for_loop_evaluation(Bool, Expr, Cmd, TempEnv, TempTempEnv),
+    update_variables(TempTempEnv, PrevEnv, Env).
+
+command_evaluation(for_loop(for, '(', Dec, Bool, ;, Expr, ')', Cmd, Cmd1), PrevEnv, Env):-
+    declaration_evaluation(Dec, PrevEnv, TempEnv),
+    for_loop_evaluation(Bool, Expr, Cmd, TempEnv, TempTempEnv),
+    update_variables(TempTempEnv, PrevEnv, TempTempTempEnv),
+    command_evaluation(Cmd1, TempTempTempEnv, Env).
+
+command_evaluation(for_loop(for, '(', Asg, ;, Bool, ;, Expr, ')', Cmd), PrevEnv, Env):-
+    assignment_evalutation(Asg, PrevEnv, TempEnv),
+    for_loop_evaluation(Bool, Expr, Cmd, TempEnv, TempTempTempEnv),
+    update_variables(TempTempTempEnv, PrevEnv, Env).
+
+command_evaluation(for_loop(for, '(', Asg, ;, Bool, ;, Expr, ')', Cmd, Cmd1), PrevEnv, Env):-
+    assignment_evalutation(Asg, PrevEnv, TempEnv),
+    for_loop_evaluation(Bool, Expr, Cmd, TempEnv, TempTempEnv),
+    update_variables(TempTempEnv, PrevEnv, TempTempTempEnv),
+    command_evaluation(Cmd1, TempTempTempEnv, Env).
+
+command_evaluation(for_loop_range(variable(Iden), in, range, number(From), number(To), number(Jump), Cmd), PrevEnv, Env) :-
+    \+ member((Iden, var, int, _), PrevEnv),
+    for_loop_range_evaluation(Iden, From, To, Jump, Cmd, [(Iden, var, int, From) | PrevEnv], TempEnv),
+    update_variables(TempEnv, PrevEnv, Env).
+
+command_evaluation(for_loop_range(variable(Iden), in, range, number(From), number(To), number(Jump), Cmd), PrevEnv, Env) :-
+    member((Iden, var, int, _), PrevEnv),
+    select((Iden, var, int, _), PrevEnv, TempEnv),
+    for_loop_range_evaluation(Iden, From, To, Jump, Cmd, [(Iden, var, int, From) | TempEnv], TempTempEnv),
+    update_variables(TempTempEnv, [(Iden, var, int, From) | TempEnv], Env).
+
+command_evaluation(for_loop_range(Iden, _, _, _, _, _, _), PrevEnv, _) :-
+    member((Iden, var, Type, _), PrevEnv),
+    Type \= int,
+    writeln('Error: Iden should be of type int').
+
+command_evaluation(for_loop_range(variable(Iden), in, range, number(From), number(To), number(Jump), Cmd, Cmd1), PrevEnv, Env) :-
+    \+ member((Iden, var, int, _), PrevEnv),
+    for_loop_range_evaluation(Iden, From, To, Jump, Cmd, [(Iden, var, int, From) | PrevEnv], TempEnv),
+    update_variables(TempEnv, PrevEnv, TempTempEnv),
+    command_evaluation(Cmd1, TempTempEnv, Env).
+
+command_evaluation(for_loop_range(variable(Iden), in, range, number(From), number(To), number(Jump), Cmd, Cmd1), PrevEnv, Env) :-
+    member((Iden, var, int, _), PrevEnv),
+    select((Iden, var, int, _), PrevEnv, TempEnv),
+    for_loop_range_evaluation(Iden, From, To, Jump, Cmd, [(Iden, var, int, From) | TempEnv], TempTempEnv),
+    update_variables(TempTempEnv, [(Iden, var, int, From) | TempEnv], TempTempTempEnv),
+    command_evaluation(Cmd1, TempTempTempEnv, Env).
+
+command_evaluation(for_loop_range(Iden, _, _, _, _, _, _, _), PrevEnv, _) :-
+    member((Iden, var, Type, _), PrevEnv),
+    Type \= int,
+    writeln('Error: Iden should be of type int').
+
+
+command_evaluation(for_loop_range_single(variable(Iden), in, range, number(To), Cmd), PrevEnv, Env) :-
+    \+ member((Iden, var, int, _), PrevEnv),
+    for_loop_range_evaluation(Iden, 0, To, 1, Cmd, [(Iden, var, int, 0) | PrevEnv], TempEnv),
+    update_variables(TempEnv, PrevEnv, Env).
+
+command_evaluation(for_loop_range_single(variable(Iden), in, range, number(To), Cmd), PrevEnv, Env) :-
+    member((Iden, var, int, _), PrevEnv),
+    select((Iden, var, int, _), PrevEnv, TempEnv),
+    for_loop_range_evaluation(Iden, 0, To, 1, Cmd, [(Iden, var, int, 0) | TempEnv], TempTempEnv),
+    update_variables(TempTempEnv, [(Iden, var, int, 0) | TempEnv], Env).
+
+command_evaluation(for_loop_range_single(Iden, _, _, _, _, _), PrevEnv, _) :-
+    member((Iden, var, Type, _), PrevEnv),
+    Type \= int,
+    writeln('Error: Iden should be of type int').
+
+command_evaluation(for_loop_range_single(variable(Iden), in, range, number(To), Cmd, Cmd1), PrevEnv, Env) :-
+    \+ member((Iden, var, int, _), PrevEnv),
+    for_loop_range_evaluation(Iden, 0, To, 1, Cmd, [(Iden, var, int, 0) | PrevEnv], TempEnv),
+    update_variables(TempEnv, PrevEnv, TempTempEnv),
+    command_evaluation(Cmd1, TempTempEnv, Env).
+
+command_evaluation(for_loop_range_single(variable(Iden), in, range, number(To), Cmd, Cmd1), PrevEnv, Env) :-
+    member((Iden, var, int, _), PrevEnv),
+    select((Iden, var, int, _), PrevEnv, TempEnv),
+    for_loop_range_evaluation(Iden, 0, To, 1, Cmd, [(Iden, var, int, 0) | TempEnv], TempTempEnv),
+    update_variables(TempTempEnv, [(Iden, var, int, 0) | TempEnv], TempTempTempEnv),
+    command_evaluation(Cmd1, TempTempTempEnv, Env).
+
+command_evaluation(for_loop_range_single(Iden, _, _, _, _, _, _), PrevEnv, _) :-
+    member((Iden, var, Type, _), PrevEnv),
+    Type \= int,
+    writeln('Error: Iden should be of type int').
 
 command_evaluation(command_block(Block), PrevEnv, Env) :-
     block_evaluation(Block, PrevEnv, Env).
 
-for_loop_evaluation(Bool, Expr, Cmd, PrevEnv, Env) :-
-    boolean_evaluation(Bool, PrevEnv, TempEnv, true),
-    command_evaluation(Cmd, TempEnv, TempTempEnv),
-    eval_expression(Expr, TempTempEnv, TempTempTempEnv, _),
-    for_loop_evaluation(Bool, Expr, Cmd, TempTempTempEnv, Env).
+% for_loop_range_evaluation(Iden, Current, To, Jump, Cmd, PrevEnv, Env) :-
+%     Current < To,
+%     command_evaluation(Cmd, PrevEnv, PrevEnv),
+%     NewCurrent is Current + Jump,
+%     update_iden_value(Iden, NewCurrent, PrevEnv, UpdatedEnv),
+%     for_loop_range_evaluation(Iden, NewCurrent, To, Jump, Cmd, UpdatedEnv, Env).
 
-for_loop_evaluation(Bool, _, _, PrevEnv, Env) :-
-    boolean_evaluation(Bool, PrevEnv, Env, false).
+for_loop_range_evaluation(Iden, Current, To, Jump, Cmd, PrevEnv, Env) :-
+    Current < To,
+    evaluate_command_env(Cmd, PrevEnv, TempEnv),
+    NewCurrent is Current + Jump,
+    update_iden_value(Iden, NewCurrent, TempEnv, UpdatedEnv),
+    update_variables(UpdatedEnv, PrevEnv, TempTempTempTempEnv),
+    for_loop_range_evaluation(Iden, NewCurrent, To, Jump, Cmd, TempTempTempTempEnv, Env).
+
+for_loop_range_evaluation(_, Current, To, _, _, Env, Env) :-
+    Current >= To.
+
+update_iden_value(Iden, Value, Env, [(Iden, var, int, Value) | TempEnv]) :-
+    select((Iden, var, int, _), Env, TempEnv).
+
+for_loop_evaluation(Bool, Expr, Cmd, PrevEnv, Env) :-
+    evaluate_boolean_env(Bool, PrevEnv, TempEnv, BoolValue),
+    (   BoolValue == true
+    ->  evaluate_command_env(Cmd, TempEnv, TempTempEnv),
+        evaluate_extract(Expr, TempTempEnv, TempTempTempEnv, _),
+        update_variables(TempTempTempEnv, PrevEnv, TempTempTempTempEnv),
+        for_loop_evaluation(Bool, Expr, Cmd, TempTempTempTempEnv, Env)
+    ;   Env = PrevEnv
+    ).
+
+% for_loop_evaluation(Bool, _, _, PrevEnv, PrevEnv) :-
+%     evaluate_boolean_env(Bool, PrevEnv, PrevEnv, false).
 
 
 ternary_expression(Expr) --> simple_expression(Expr).
@@ -255,6 +394,10 @@ booleanCondition(boolean_negation(not, B)) --> ['not'], booleanCondition(B).
 booleanCondition(boolean_negation(!, B)) --> ['!'], booleanCondition(B).
 booleanCondition(boolean_not_equal(E, '!=', E1)) --> simple_expression(E), ['!', '='], simple_expression(E1).
 
+evaluate_boolean_env(Bool, PrevEnv, Env, Result) :-
+    boolean_evaluation(Bool, PrevEnv, TempEnv, Result),
+    (var(TempEnv) -> Env = PrevEnv; Env = TempEnv).
+
 boolean_evaluation(boolean_true(true), _, _, true).
 boolean_evaluation(boolean_false(false), _, _, false).
 boolean_evaluation(boolean_equals(E, == ,E1), PrevEnv, Env, Result) :-
@@ -282,10 +425,10 @@ boolean_evaluation(boolean_less_equal(E, <= ,E1), PrevEnv, Env, Result) :-
     (Val =< Val1 -> Result = true
     ; Result = false).
 boolean_evaluation(boolean_negation(not, B), PrevEnv, Env, Result) :-
-    boolean_evaluation(B, PrevEnv, Env,TempResult),
+    evaluate_boolean_env(B, PrevEnv, Env,TempResult),
     negate(TempResult, Result).
 boolean_evaluation(boolean_negation(!, B), PrevEnv, Env, Result) :-
-    boolean_evaluation(B, PrevEnv, Env, TempResult),
+    evaluate_boolean_env(B, PrevEnv, Env, TempResult),
     negate(TempResult, Result).
 boolean_evaluation(boolean_not_equal(E, '!=' ,E1), PrevEnv, Env, Result) :-
     eval_expression(E, PrevEnv, Env, Val),
@@ -323,7 +466,7 @@ get_value(I, PrevEnv, Val) :-
 
 eval_expression(number(Num), _, _, Num).
 eval_expression(string_(Str), _, _, Str).
-eval_expression(variable(Iden), PrevEnv, _, Value) :- 
+eval_expression(variable(Iden), PrevEnv, _, Value) :-
     (get_value(Iden, PrevEnv, Value) -> true; throw(error(undeclared_variable(Iden)))).
 
 eval_expression(add(E1, E2), PrevEnv, Env, Result) :-
@@ -336,21 +479,21 @@ eval_expression(add(E1, E2), PrevEnv, Env, Result) :-
 eval_expression(substract(E1, E2), PrevEnv, Env, Result) :-
     eval_expression(E1, PrevEnv, Env, R1),
     eval_expression(E2, PrevEnv, Env, R2),
-    (check_same_datatype(R1, R2) -> 
+    (check_same_datatype(R1, R2) ->
         Result is R1 - R2
     ; Result = throw(error(type_mismatch))).
 
 eval_expression(multiply(E1, E2), PrevEnv, Env, Result) :-
     eval_expression(E1, PrevEnv, Env, R1),
     eval_expression(E2, PrevEnv, Env, R2),
-    (check_same_datatype(R1, R2) -> 
+    (check_same_datatype(R1, R2) ->
         Result is R1 * R2
     ; Result = throw(error(type_mismatch))).
 
 eval_expression(divide(E1, E2), PrevEnv, Env, Result) :-
     eval_expression(E1, PrevEnv, Env, R1),
     eval_expression(E2, PrevEnv, Env, R2),
-    (check_same_datatype(R1, R2) -> 
+    (check_same_datatype(R1, R2) ->
         (R2 =:= 0 -> throw(error(divide_by_zero)); Result is R1 / R2)
     ; Result = throw(error(type_mismatch))).
 
